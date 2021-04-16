cd7c5a4ecc99fe740fa9ad12133d6e607478fa07
Stephen Bowen <sfbowen4@gmail.com>
2021-04-16 01:02:44 -0500

    Added long press to loop commands in iOS application

---
+++ Spot For iOS/Spot For iOS/MovementButton.swift
@@ -0,0 +1,20 @@
+//
+//  MovementButton.swift
+//  Spot For iOS
+//
+//  Created by Stephen Bowen on 4/15/21.
+//
+
+import SwiftUI
+
+struct MovementButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MovementButton_Previews: PreviewProvider {
+    static var previews: some View {
+        MovementButton()
+    }
+}
